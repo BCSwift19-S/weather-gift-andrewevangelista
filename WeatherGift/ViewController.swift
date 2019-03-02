9f6fc5851480d4ab3614b14ef2bb7448c01f855c
Andrew <andrewevangelista@andrew-evangelista-smbp.local>
2019-03-01 21:53:21 -0500

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Andrew on 3/1/19.
+//  Copyright © 2019 Andrew Evangelista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
