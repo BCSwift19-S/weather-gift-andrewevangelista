e3141a4b77cd7467a7485617c7869b674e50ab63
Andrew <andrewevangelista@andreweistasmbp.hsd1.ma.comcast.net>
2019-03-15 15:46:14 -0400

    7.10 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Andrew on 3/15/19.
+//  Copyright © 2019 Andrew Evangelista. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+    
+}
