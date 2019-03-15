382b826eb602a4ce4aae65c245cd9b57f94aaaf4
Andrew <andrewevangelista@andreweistasmbp.hsd1.ma.comcast.net>
2019-03-15 18:23:25 -0400

    7.12 Complete!

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -21,6 +21,7 @@ class DetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         locationLabel.text = locationsArray[currentPage].name
+        dateLabel.text = locationsArray[currentPage].coordinates
         
     }
     
